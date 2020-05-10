97e620399d47b630611af90aebc1c30897713abd
Neil Leon <neilleon22@gmail.com>
2020-05-10 02:20:43 -0400

    Initial Commit

---
+++ MCCapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MCCapp
+//
+//  Created by Xcode Mac on 5/10/20.
+//  Copyright © 2020 MCCTechMinistry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
